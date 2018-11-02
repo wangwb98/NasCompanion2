24d6164945aa8b662b9e247a3af940d08b60c566
Wilbur Wang <wolfwang@gmail.com>
2018-11-02 17:44:09 +0800

    use android-jobs background management, and use broadcast intent to update UI

--- app/src/main/java/com/asr/nascompanion/MainActivity.kt
+++ app/src/main/java/com/asr/nascompanion/MainActivity.kt
@@ -20,19 +20,18 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
 import android.support.v4.app.ActivityCompat
+import android.support.v4.content.LocalBroadcastManager
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import com.evernote.android.job.Job
 import com.evernote.android.job.JobCreator
 import com.evernote.android.job.JobManager
 import com.evernote.android.job.JobRequest
-
-import jcifs.smb.NtlmPasswordAuthentication
-import jcifs.smb.SmbFile
-import java.io.File
-import java.util.*
+import jcifs.smb.*
 
 import kotlinx.android.synthetic.main.content_main.*
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 class NasCompanionApp : Application() {
     override fun onCreate() {
@@ -54,27 +53,40 @@ class NasSyncJob : Job() {
     companion object {
         const val TAG = "nas_file_sync"
         fun scheduleJob() {
+            val allRequests = JobManager.instance().getAllJobRequestsForTag(NasSyncJob.TAG)
+            if (!allRequests.isEmpty()) {
+                Log.d(TAG, "already running jobs, skip this time's request")
+                return
+            }
+
             JobRequest.Builder(NasSyncJob.TAG)
-                .setExecutionWindow(1_000L, 4_000L)
+                .setUpdateCurrent(true)
+                .setRequiredNetworkType(JobRequest.NetworkType.UNMETERED)
+                .setRequirementsEnforced(true)
+                .setPeriodic(TimeUnit.HOURS.toMillis(3), TimeUnit.HOURS.toMillis(3)) // every 3 hours, but wait 6 hours before it runs again
+                .build()
+                .schedule()
+        }
+        fun runJobImmediatelly() {
+            // JobManager.instance().cancelAllForTag(NasSyncJob.TAG)
+            JobRequest.Builder(NasSyncJob.TAG)
+                .setUpdateCurrent(true)
+                .startNow()
                 .build()
                 .schedule()
         }
     }
     override fun onRunJob(params: Params): Result {
         // run our job here
-        listMediaFiles()
+        NasSyncMediaFiles()
         return Result.SUCCESS
     }
-    private fun listMediaFiles(): Pair<Long, String>? {
-        val intent = Intent()
-        intent.action = "test"
-        intent.flags = Intent.FLAG_INCLUDE_STOPPED_PACKAGES
-
+    private fun NasSyncMediaFiles(): Pair<Long, String>? {
         val CAMERA_IMAGE_BUCKET_NAME = Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera"
         val CAMERA_IMAGE_BUCKET_ID = getBucketId(CAMERA_IMAGE_BUCKET_NAME)
         val projection = arrayOf(MediaStore.Images.Media.DATA, MediaStore.Images.Media.DATE_MODIFIED, MediaStore.Images.Media.DISPLAY_NAME)
-        val selection = MediaStore.Images.Media.BUCKET_ID + " = ?"
-        val selectionArgs = arrayOf(CAMERA_IMAGE_BUCKET_ID)
+/*        val selection = MediaStore.Images.Media.BUCKET_ID + " = ?"
+        val selectionArgs = arrayOf(CAMERA_IMAGE_BUCKET_ID)*/
         var cameraPair: Pair<Long, String>? = null
 
         var cursor = this.context.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
@@ -88,10 +100,10 @@ class NasSyncJob : Job() {
                 cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA)))
         }
         do {
-            Log.d(TAG, cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA)))
-            //copyToNas(cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA)))
-
-            this.context.sendBroadcast(intent)
+            val n_list = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA)).split("/")
+            val picName = n_list[n_list.lastIndex-1]+"/"+n_list[n_list.lastIndex]
+            Log.d(TAG, picName)
+            copyToNas(cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA)))
         } while(cursor.moveToNext())
         //toast(cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA)))
         Log.d(TAG,cursor.count.toString())
@@ -101,16 +113,66 @@ class NasSyncJob : Job() {
         return path.toLowerCase().hashCode().toString()
     }
 
+    private fun copyToNas(path: String): Boolean {
+        val intent = Intent()
+        intent.action = "com.asr.nascompanion.updateStatus"
+
+        val server_addr = "smb://192.168.0.2/public/"+Build.MODEL
+        val n_list = path.split("/")
+        val folder_name = n_list[n_list.lastIndex-1]
+        val file_name = n_list[n_list.lastIndex]
+
+        var fileResult = "Done"
+
+        Log.d(TAG, "To create file " + server_addr +"/"+folder_name+"/"+file_name)
+/*      test to write local file.
+        val to_file = openFileOutput("test.test", MODE_PRIVATE)
+        from_file.copyTo(to_file)*/
+
+        intent.putExtra("msg", "\nCopying "+folder_name+"/"+file_name+"...")
+        LocalBroadcastManager.getInstance(this.context).sendBroadcastSync(intent)
+
+        try {
+            var smb_path = SmbFile(server_addr, NtlmPasswordAuthentication.ANONYMOUS)
+            if (!smb_path.exists()) {
+                smb_path.mkdir()
+            }
+            smb_path = SmbFile(server_addr + "/" + folder_name, NtlmPasswordAuthentication.ANONYMOUS)
+            if (!smb_path.exists()) {
+                smb_path.mkdir()
+            }
+            smb_path = SmbFile(server_addr + "/" + folder_name + "/" + file_name, NtlmPasswordAuthentication.ANONYMOUS)
+            if (!smb_path.exists()) {
+                val from_file = File(path).inputStream()
+                from_file.use { input ->
+                    smb_path.outputStream.use { output ->
+                        input.copyTo(output)
+                    }
+                }
+                Log.d(TAG, from_file.available().toString())
+            } else {
+                fileResult = "Already exists"
+            }
+        } catch (e: SmbException) {
+            Log.d(TAG, "Samba connection met issue:"+e.localizedMessage)
+            fileResult = "Network down"
+            Thread.sleep(2000)
+        }
+        intent.putExtra("msg", fileResult)
+        LocalBroadcastManager.getInstance(this.context).sendBroadcastSync(intent)
+
+        return true
+    }
 }
 
 class MainActivity : AppCompatActivity() {
-
-    private var mBackgroundTimer: Timer? = null
-
     var broadCastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             when (intent?.action) {
-                "test" -> toast("received broadcast")
+                "com.asr.nascompanion.updateStatus" ->  {
+                    Log.d(TAG, "received Broadcast for: "+intent.getStringExtra("msg"))
+                    runOnUiThread { -> textBox.append(intent.getStringExtra("msg"))}
+                }
             }
         }
     }
@@ -120,26 +182,28 @@ class MainActivity : AppCompatActivity() {
 
         textBox.movementMethod = ScrollingMovementMethod()
 
-        registerReceiver(broadCastReceiver, IntentFilter("test"))
+        LocalBroadcastManager.getInstance(applicationContext).registerReceiver(broadCastReceiver, IntentFilter("com.asr.nascompanion.updateStatus"))
 
         setSupportActionBar(toolbar)
         toast("start running")
+
+        NasSyncJob.scheduleJob()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
             if ( checkPermission() ) {
-                NasSyncJob.scheduleJob()
-                /*
-                mBackgroundTimer?.cancel()
-                mBackgroundTimer = Timer()
-                mBackgroundTimer?.schedule(CopyToNasTask(), 0)
-                */
+                NasSyncJob.runJobImmediatelly()
             }
             else
                 longToast("No permission to read storage files. Need to be granted.")
             //applicationContext.contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LocalBroadcastManager.getInstance(applicationContext).unregisterReceiver((broadCastReceiver))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -168,48 +232,6 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-
-    private inner class CopyToNasTask () : TimerTask() {
-        override fun run() {
-            //listMediaFiles()
-        }
-    }
-
-    private fun copyToNas(path: String): Boolean {
-        val server_addr = "smb://192.168.0.2/public/"+Build.MODEL
-        val n_list = path.split("/")
-        val folder_name = n_list[n_list.lastIndex-1]
-        val file_name = n_list[n_list.lastIndex]
-        Log.d(TAG, "To create file " + server_addr +"/"+folder_name+"/"+file_name)
-/*        val to_file = openFileOutput("test.test", MODE_PRIVATE)
-        from_file.copyTo(to_file)*/
-
-        runOnUiThread { -> textBox.append("Copy "+path+" ...")}
-
-        var smb_path = SmbFile(server_addr, NtlmPasswordAuthentication.ANONYMOUS)
-        if (! smb_path.exists()) {
-            smb_path.mkdir()
-        }
-        smb_path = SmbFile(server_addr +"/"+folder_name, NtlmPasswordAuthentication.ANONYMOUS)
-        if (! smb_path.exists()) {
-            smb_path.mkdir()
-        }
-        smb_path = SmbFile(server_addr +"/"+folder_name+"/"+file_name, NtlmPasswordAuthentication.ANONYMOUS)
-        if (! smb_path.exists()) {
-            val from_file = File(path).inputStream()
-            from_file.use { input ->
-                smb_path.outputStream.use { output ->
-                    input.copyTo(output)
-                }
-            }
-            Log.d(TAG, from_file.available().toString())
-
-            runOnUiThread { -> textBox.append("Done\n") }
-        } else {
-            runOnUiThread { -> textBox.append ("Already exist\n") }
-        }
-        return true
-    }
     companion object {
         private val TAG = "NasComp"
     }
